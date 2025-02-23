8e98bea33c02610d6eb6acced4f6d883e0522aa0
Zaidoun Khalid <kalid@macmini.home>
2025-02-23 18:00:43 +0100

    Add component for rendering board

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import Board from "./components/Board.tsx";
+
 
 function App() {
 
     return (
-        <>
-
-        </>
+        <div className="relative w-screen h-screen">
+            <Board />
+        </div>
     );
 }
 
