bd9a9fecbc6a655dae4045f55eaab8c46ec52fac
Zaidoun Khalid <kalid@macmini.home>
2025-03-07 03:48:44 +0100

    Add references and update report.pdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { JSX } from "react";
 import { Board } from "./components/Board.tsx";
 import { Header } from "./components/Header.tsx";
 import { GridProvider } from "./components/GridContext.tsx";
 import { EditorProvider } from "./components/EditorContext.tsx";
 
-function App() {
+function App(): JSX.Element {
     return (
         <div className="app">
             <GridProvider>
